26c228b0947acabf0139abe00f143bb0edc56be4
aarishlakhani <alakha33@uwo.ca>
2024-09-04 21:50:32 -0400

    Strings

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,25 @@
+//strings
+console.log("hello, world");
+let email = "aarishlakhani2@gmail.com";
+console.log(email);
+
+//string concatenation
+let firstName = "Aarish";
+let lastName = "Lakhani";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+//String length
+console.log(fullName.length);
+
+//String methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+//argument
+//used to find the index of @ inside the email
+let index = email.indexOf("@");
+console.log(index);
