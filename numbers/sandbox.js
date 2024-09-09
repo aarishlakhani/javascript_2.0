25d8ce65116e3490855a17b71f63948072272bb6
aarishlakhani <alakha33@uwo.ca>
2024-09-08 21:34:13 -0400

    Numbers

--- numbers/sandbox.js
+++ numbers/sandbox.js
@@ -3,3 +3,29 @@ const pi = 3.14;
 
 //console.log(radius,pi);
 //math operators +,-,*,/,**,%
+console.log(10 / 2);
+//divides radies by 3 and gives us the remainder
+//should be 1.0466
+//shows as 1
+let result = radius % 3;
+console.log(result);
+//two ** means to the power
+//so in this case we are multiplying pi with radius square
+let result2 = pi * radius ** 2;
+console.log(result2);
+
+//order of operations- BIDMAS= Bracket, Index, Division, Multiplication, Addition and Subctraction
+let result3 = 5 * (10 - 3) ** 2;
+//my guess= 245
+console.log(result3);
+//result=245
+
+let likes = 10;
+likes = likes + 1;
+//result 11
+//likes++;
+//likes--;
+//result = 9
+//takes the current values and added 10;
+//results 20
+//likes+= 10;
