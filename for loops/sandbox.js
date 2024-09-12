2215c83d45d8e837b5a89c44d6108be85511cbbb
aarishlakhani <alakha33@uwo.ca>
2024-09-11 23:40:29 -0400

    for loop

---
+++ for loops/sandbox.js
@@ -0,0 +1,17 @@
+//for loops
+for (let i = 0; i < 5; i++) {
+  console.log("in loop: ", i);
+}
+console.log("loop finished");
+
+//array loops
+const names = ["aarish", "arshaan", "akbar"];
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+} //output 0 1 2
+
+//accessing array elements
+const names2 = ["aarish", "arshaan", "akbar"];
+for (let i = 0; i < names.length; i++) {
+  console.log(names2[i]);
+} //output aarish arshaan akbar
