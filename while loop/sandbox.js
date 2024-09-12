81ed0eb2e7d53da449fea90ed5021a174953707f
aarishlakhani <alakha33@uwo.ca>
2024-09-12 17:32:56 -0400

    while loop

---
+++ while loop/sandbox.js
@@ -0,0 +1,17 @@
+//while loop
+//same as for loop but not all different statements are together
+//only condition inside the parentheses
+
+let i = 0;
+while (i < 5) {
+  console.log("in the loop", i);
+  i++;
+}
+
+//array while loop
+const names = ["aarish", "arshaan", "akbar"];
+let j = 0;
+while (j < names.length) {
+  console.log("Hi! My name is ", names[j]);
+  j++;
+}
