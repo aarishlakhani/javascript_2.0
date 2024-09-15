48216950ccfcf10bc0a0d5b87893efd67ee015d0
aarishlakhani <alakha33@uwo.ca>
2024-09-14 21:18:09 -0400

    do while loop

---
+++ do while loop/sandbox.js
@@ -0,0 +1,10 @@
+//do while loop
+//this is to have a guaranteed execution and its a post condition loop
+//A do-while loop is a control flow statement in programming that
+//repeatedly executes a block of code at least once and
+//then continues to execute it as long as a given condition is true.
+let i = 5;
+do {
+  console.log("val of i is : ", i);
+  i++;
+} while (i < 5);
