e57646da3c1b5b8020c0415a2508fd9f344e8c43
aarishlakhani <alakha33@uwo.ca>
2024-09-15 23:57:03 -0400

    if statements

---
+++ if statements/sandbox.js
@@ -0,0 +1,15 @@
+//if statements
+const age = 25;
+if (age > 20) {
+  console.log("you are over 20 years old");
+}
+
+const ninja = ["aarish", "arshaan", "akbar"];
+if (ninja.length > 2) {
+  console.log("Hey Ninjas!");
+}
+const password = "pass";
+
+if (password.length >= 8) {
+  console.log("That password is long enough");
+}
