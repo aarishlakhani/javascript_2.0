f7247573b40d9de1e8f6a5f380b5ff1ce76d5a8f
aarishlakhani <alakha33@uwo.ca>
2024-09-16 22:59:04 -0400

    logical not

---
+++ logical not/sandbox.js
@@ -0,0 +1,10 @@
+//logical NOT operators (!)
+//if added before any variable/boolean it would result in an opposite value
+
+let user = false;
+
+if (!user) {
+  console.log("you must be logged in to continue");
+}
+console.log(!true); //results in false
+console.log(!false); //results in true
