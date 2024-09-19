150947f87e9586680e5196ec3a654765be06129e
aarishlakhani <alakha33@uwo.ca>
2024-09-18 23:56:49 -0400

    swtich statements

---
+++ switch statement/sandbox.js
@@ -0,0 +1,22 @@
+//switch statement
+const grade = "D";
+
+switch (grade) {
+  case "A":
+    console.log("You got an A!");
+    break;
+  case "B":
+    console.log("You got a B.");
+    break;
+  case "C":
+    console.log("You got a C.");
+    break;
+  case "D":
+    console.log("You got a D.");
+    break;
+  case "F":
+    console.log("You got a F.");
+    break;
+  default:
+    console.log("Not a valid grade");
+}
